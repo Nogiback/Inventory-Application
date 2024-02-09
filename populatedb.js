d60e400d2aac135de57347220c80b314d79589a7
Nogiback <peter.toan.do@gmail.com>
2024-02-08 21:37:12 -0500

    Removing old comment

--- populatedb.js
+++ populatedb.js
@@ -33,9 +33,6 @@ async function main() {
   mongoose.connection.close();
 }
 
-// We pass the index to the ...Create functions so that, for example,
-// genre[0] will always be the Fantasy genre, regardless of the order
-// in which the elements of promise.all's argument complete.
 async function manufacturerCreate(index, name, plastics) {
   const manufacturer = new Manufacturer({
     name: name,
